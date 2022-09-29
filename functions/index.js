399f948a5069dc454216e4cf63c34b3cd807bab4
Patrick Huynh <pldhuynh@gmail.com>
2022-09-29 17:58:46 +1000

    added cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const auth = require("./util/auth");
+const cors = require("cors")({ origin: true });
 
 const { getAllObjects, getObject, postObject, deleteObject, editObject } = require("./APIs/objects");
 const { loginUser, signUpUser } = require("./APIs/users");
@@ -13,5 +14,7 @@ app.put("/objects/:objectId", auth, editObject);
 
 app.post("/login", loginUser);
 app.post("/signup", signUpUser);
+
+app.use(cors);
 
 exports.api = functions.https.onRequest(app);
